ec7a9c67190466da77f16c0a85bce5e92539148f
defnotaturtle <defnotaturtle@tutamail.com>
2026-09-16 14:33:29 +0000

    Refactor draw function and update FPS display logic

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 const canvas = document.getElementById("gameCanvas");
 const canvasContext = canvas.getContext("2d");
 
-const message = "hello world";
 const target_frame_rate = 60;
 const draw_time_target = 1000 / target_frame_rate;
 
 let draw_count = 0;
-
-let game_over = false;
 let last_draw_time = performance.now();
 let current_time = Date.now();
 
-
-// Example draw
+// game loop?
 function draw() {
-
-
 
   // draw/update count
   draw_count++;
@@ -26,29 +20,33 @@ function draw() {
   delta_time = performance.now() - last_draw_time;
   last_draw_time = performance.now();
 
-
-
   // clear
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
 
-
-  
-  // draw FPS
-  const fps = (1000 / delta_time).toFixed(2);
-  canvasContext.fillStyle = "black";
-  canvasContext.font = "8px Arial";
-  canvasContext.fillText(`FPS: ${fps}`, 10, 75);
-
+  // draw FPS / time stats
+  // draw_time_stats();
+  draw_fps(canvas.width - 45, 10);
 
   // rec example
   canvasContext.fillStyle = "red";
   canvasContext.fillRect(0, 0, 10, 10);
 
+  requestAnimationFrame(draw);
+}
+
+// starts game loop
+draw();
+
+
+
+// function things
+
+function draw_time_stats() {
+
   // show draw_target_time
   canvasContext.fillStyle = "black";
   canvasContext.font = "8px Arial";
   canvasContext.fillText(`Draw target time: ${draw_time_target.toFixed(2)} ms`, 10, 60);
-
 
   // show draw count
   canvasContext.fillStyle = "black";
@@ -66,17 +64,12 @@ function draw() {
   // format current time as a human-readable string
   const current_time_string = new Date(current_time).toLocaleTimeString();
   canvasContext.fillText(`Current time: ${current_time_string}`, 10, 45);
-
-
-
-
-  requestAnimationFrame(draw);
 }
 
-// game loop
-// while (!game_over) {
+function draw_fps(x = 10, y = 75) {
+  const fps = (1000 / delta_time).toFixed(2);
+  canvasContext.fillStyle = "black";
+  canvasContext.font = "8px Arial";
+  canvasContext.fillText(`FPS: ${fps}`, x, y);
+}
 
-// }
-
-
-draw();
