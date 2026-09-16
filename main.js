3ceb257cc38940b30a2af8b73f76f6f7be4d3680
defnotaturtle <defnotaturtle@tutamail.com>
2026-09-16 13:45:02 +0000

    cleanup

---
+++ main.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById("gameCanvas");
+const canvasContext = canvas.getContext("2d");
+
+const message = "hello world";
+
+// Example draw
+function draw() {
+  // clear
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+  // rec example
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(100, 100, 200, 200);
+
+  requestAnimationFrame(draw);
+}
+
+
+draw();
