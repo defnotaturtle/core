8320a9973e714f3bb04d76bdecd4e8efeb2efc7a
defnotaturtle <defnotaturtle@tutamail.com>
2026-09-26 15:18:16 +0000

    bump

--- react_core/src/App.jsx
+++ react_core/src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css";
 
 const App = () => {
   // local state
+  const [prime_info, set_prime_info] = useState({
+    prime_candidates : [2],
+    prime_multiples : [2]
+  });
+
   const [candidates, setCandidates] = useState([2]);
+  const [candidates_diff, setCandidates_diff] = useState([2]);
 
   const step_primes = () => {
     // // increment candidates
@@ -11,6 +17,34 @@ const App = () => {
       const new_candidate = currentCandidates[currentCandidates.length - 1] + 1;
       return [...currentCandidates, new_candidate];
     });
+
+    
+    setCandidates_diff((currentCandidates_diff) => {
+      const new_candidate_diff = currentCandidates_diff[currentCandidates_diff.length - 1] + 1;
+      return [...currentCandidates_diff, new_candidate_diff];
+    });
+
+    set_prime_info((current_info) => {
+      
+      // const new_prime_info = {
+      //   prime_candidates : [2],
+      //   prime_multiples : [4]
+      // };
+      if (current_info && current_info.prime_candidates) {
+              const new_candidate = current_info[current_info.prime_candidates.length - 1] + 1;
+ 
+        return {
+          prime_candidates : [...current_info.prime_candidates, 1],
+          prime_multiples : [...current_info.prime_multiples]
+        };
+       
+      
+      }
+
+
+      // return new_prime_info
+    });
+
   };
 
   // setup game_loop via requestAnimationFrame
@@ -43,6 +77,22 @@ const App = () => {
         ))}
       </div>
       <div>{candidates[candidates.length - 1]}</div>
+
+            <div>
+        {candidates_diff.slice(0, 15).map((candidate_diff, index) => (
+          <div key={index}>{candidate_diff}</div>
+        ))}
+      </div>
+      <div>{candidates_diff[candidates_diff.length - 1]}</div>
+     
+            <div>
+              heyo
+        {prime_info.prime_candidates?.slice(0, 15).map((prime_candidate, index) => (
+          <div key={index}>{prime_candidate} | none</div>
+        ))}
+      </div>
+      {/* <div>{candidates_diff[candidates_diff.length - 1]}</div> */}
+
     </>
   );
 };
